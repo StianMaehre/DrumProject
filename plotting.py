91ab7081756b0927b93b2e90e2838f465e9e6905
StianMaehre <123373297+StianMaehre@users.noreply.github.com>
2025-06-19 00:53:18 +0200

    Minor fixes to plotting

--- plotting.py
+++ plotting.py
@@ -22,11 +22,11 @@ def plotSolution(mode, kochSquare, Ll, L, title):
     plt.plot(kochSquare[:, 0], kochSquare[:, 1], 'k-', linewidth=0.5)
     plt.colorbar(label='Amplitude')
     plt.axis("off")
-    plt.show()
     try:
-        plt.savefig(f"plots/{title}.png", bbox_inches='tight')
+        plt.savefig(f"./plots/{title}.png", bbox_inches='tight')
     except FileNotFoundError:
         print("Directory 'plots' does not exist. Please create it to save the plots.")
+    plt.show()
     
 
 def plotEigenvalues(eigenvalues, eigenvectors, kochSquare, Ll, L):
